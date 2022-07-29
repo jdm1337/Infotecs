1dd7d22b6a80b32e2c8438eb0fae30f1389031a6
Sergey <sstaramaly@gmail.com>
2022-07-29 11:58:54 +0300

    Add copying

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace Task1;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Task1;
 
 public class Program
 {
     public static void Main(string [] args)
     {
+        try 
+        {
+            var host = CreateDefaultBuilder().Build();
 
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            var provider = serviceScope.ServiceProvider;
+
+            var copierInstance = provider.GetRequiredService<CopyMaster>();
+            copierInstance.Execute();
+
+            host.Run();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static IHostBuilder CreateDefaultBuilder()
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration(app =>
+            {
+                app.AddJsonFile("appsettings.json");
+            })
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<CopyMaster>();
+            });
     }
 }

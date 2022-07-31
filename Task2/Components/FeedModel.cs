dbe3a160ac2b88017eb64cb66e47da979295e2c3
Sergey <sstaramaly@gmail.com>
2022-08-01 01:05:35 +0300

    Move implementation to behind code level

---
+++ Task2/Components/FeedModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Task2.Components
+{
+    public class FeedModel : ComponentBase
+    {
+        public SyndicationFeed Feed { get; set; }
+        private readonly IConfiguration _configuration;
+        public FeedModel(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            var url = "https://habr.com/ru/rss/interesting/";
+            using var reader = XmlReader.Create(url);
+            Feed = SyndicationFeed.Load(reader);
+        }
+    }
+}
---
+++ Task2/Services/FeedService.cs
@@ -0,0 +1,25 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace Task2.Services
+{
+    public class FeedService
+    {
+        public SyndicationFeed Feed { get; set; }
+        private string RssUrl { get; set; }
+        private int UpdateInterval { get; set; }
+        private readonly IConfiguration _configuration;
+
+        public FeedService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            RssUrl = configuration.GetSection("RssPath").Value;
+            UpdateInterval = Convert.ToInt32(configuration.GetSection("UpdateInterval").Value);
+        }
+        protected async Task UpdateDataAsync()
+        {
+            using var reader = XmlReader.Create(RssUrl);
+            Feed = SyndicationFeed.Load(reader);
+        }
+    }
+}
